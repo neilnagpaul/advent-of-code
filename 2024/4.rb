70b232905cf0218df07d5e58f441d4c5b36e29bb
neilnagpaul <65032886+neilnagpaul@users.noreply.github.com>
2024-12-11 17:58:50 -0500

    Update 4.rb

--- 2024/4.rb
+++ 2024/4.rb
@@ -1,21 +1,14 @@
-$lines = ARGF.read.split
+require 'matrix'
+@grid = Matrix[*ARGF.map { _1.strip.chars }]
 directions = [-1, 0, 1]
-test = [%w[M A S], %w[S A M]]
+deltas = *directions.repeated_permutation(2)
+def dig(i, j) = (@grid[i, j] if [i, j].none?(&:negative?))
 
-def dig(y, x)
-    $lines[y][x] if (0...$lines.size) === y && (0...$lines[0].size) === x
-end
-
-p (0...$lines.size).sum { |y|
-    (0...$lines[0].size).sum { |x|
-        directions.product(directions).count { |dy, dx|
-            (0...4).map { |i| dig(y + dy * i, x + dx * i) } == %w[X M A S]
-        }
-    }
+p @grid.each_with_index.to_a.product(deltas).count { |(_, i, j), (di, dj)|
+    (0...4).map { |n| dig(i + di * n, j + dj * n) }.join == 'XMAS'
 }
-p (0...$lines.size).sum { |y|
-    (0...$lines[0].size).count { |x|
-        test.include?(directions.map { |d| dig(y+d, x-d) }) &&
-        test.include?(directions.map { |d| dig(y+d, x+d) })
-    }
+p @grid.each_with_index.count { |_, i, j|
+    [directions.map { |d| dig(i+d, j-d) }.join,
+     directions.map { |d| dig(i+d, j+d) }.join]
+      .all?(Set['MAS', 'SAM'])
 }
