db3705b428558adb016aa60de872e36306f84fc9
neilnagpaul <65032886+neilnagpaul@users.noreply.github.com>
2024-12-24 20:01:08 -0500

    Day 22

---
+++ 2024/22.rb
@@ -0,0 +1,13 @@
+succ = ->(seed) {
+  seed = (seed * 64 ^ seed) % 16777216
+  seed = (seed / 32 ^ seed) % 16777216
+  seed = (seed * 2048 ^ seed) % 16777216
+  seed
+}
+seq = ARGF.map { |n| 2000.times.inject([n.to_i]) { _1 << succ[_1.last] } }
+p seq.sum(&:last)
+
+seq.map! { |steps| steps.map { _1 % 10 } }
+sell = ->(cons) { [cons.each_cons(2).map { _2 - _1 }, cons.last] }
+bananas = ->(steps) { steps.each_cons(5).map(&sell).uniq(&:first) }
+p seq.flat_map(&bananas).group_by(&:first).map { _2.sum(&:last) }.max
