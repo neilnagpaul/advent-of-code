cb0f94957cb41ca939be4026478f5dc5e6ee7ece
neilnagpaul <65032886+neilnagpaul@users.noreply.github.com>
2024-12-26 22:36:06 -0500

    Days 17 through 18

---
+++ 2024/17.rb
@@ -0,0 +1,36 @@
+def run(program, regs)
+  a, b, c = 4, 5, 6
+  ip = 0
+  combo = [0, 1, 2, 3] + regs
+  out = []
+  while ip < program.length
+    opcode, operand = program[ip, 2]
+    case opcode
+    when 0 then combo[a] >>= combo[operand]
+    when 1 then combo[b] ^= operand
+    when 2 then combo[b] = combo[operand] % 8
+    when 3 then ip = operand - 2 if combo[a] != 0
+    when 4 then combo[b] ^= combo[c]
+    when 5 then out << (combo[operand] % 8)
+    when 6 then combo[b] = combo[a] >> combo[operand]
+    when 7 then combo[c] = combo[a] >> combo[operand]
+    end
+    ip += 2
+  end
+  out
+end
+
+def expect(program, out, prev = 0)
+  return prev if out.empty?
+  (1 << 10).times { |a|
+    next unless (a >> 3 == prev & 127) &&
+      (run(program, [a, 0, 0])[0] == out[-1])
+    ret = expect(program, out[...-1], (prev << 3) | (a % 8))
+    return ret if ret
+  } and nil
+end
+
+nums = ARGF.read.scan(/\d+/).map(&:to_i)
+regs, program = nums[0..2], nums[3..]
+puts run(program, regs).to_a * ","
+puts expect(program, program)
---
+++ 2024/18.rb
@@ -0,0 +1,19 @@
+require "matrix"
+@data = ARGF.map { Vector[*it.split(",").map(&:to_i)] }
+@deltas = [[1, 0], [0, 1], [-1, 0], [0, -1]].map { Vector[*it] }
+
+def path(i)
+  seen = @data[...i].to_set
+  queue = [[0, Vector[0, 0]]]
+  queue.each { |dist, pos|
+    return dist if pos == Vector[70, 70]
+    @deltas.map { pos + it }.select { [*it] in 0..70, 0..70 }.each {
+      next if seen === it
+      queue << [dist + 1, it]
+      seen << it
+    }
+  } and nil
+end
+
+p path 1024
+puts @data[@data.each_index.to_a.bsearch { !path(it) } - 1].to_a * ","
